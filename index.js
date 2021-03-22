e73607b609b6262bcc1c5a0522556d7611925f52
Sophie Shin <sophie@alloy.co>
2021-03-22 15:16:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function breakOut(array, changeValue, stopValue) {
   for (let i =0; i<array.length; i++) {
     if (array[i] !== stopValue) {
       array[i] = changeValue
+      break
     }
   }
 return array
