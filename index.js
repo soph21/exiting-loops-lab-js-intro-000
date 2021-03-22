8ce5aed199ebb86af3a8d11d67135602eac91fb3
Sophie Shin <sophie@alloy.co>
2021-03-22 15:52:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,8 +22,7 @@ function findBy(array, findFn) {
   for (let i = 0; i <array.length; i++) {
     if (findFn(array[i])) {
       return array[i]
-   } else {
-      return null
+   }
     }
-  return array
+  return null
 }
